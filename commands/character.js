d327f3df76517e946eb35fe55469ee71ce4f1d1e
Giselle Domingo <giselle.domingo.x@gmail.com>
2022-11-16 10:40:59 -0500

    Added ability to search characters via slash command

--- commands/character.js
+++ commands/character.js
@@ -1,30 +1,36 @@
-//https://www.moogleapi.com/api/v1/characters/search?name=lightning
-// this code is a test run that searches for one character
-const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder} = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  AttachmentBuilder,
+} = require("discord.js");
 const { request } = require("undici");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("character")
-    .setDescription("Gets Lightning info."),
+    .setDescription("Gets Lightning info.")
+    .addStringOption(option => 
+      option.setName("name").setDescription("character name").setRequired(false)
+    ),
   async execute(interaction) {
+    const name = interaction.options.getString("name");
+    const query = new URLSearchParams({ name });
     const { body } = await request(
-      "https://www.moogleapi.com/api/v1/characters/search?name=lightning"
+      `https://www.moogleapi.com/api/v1/characters/search?${query}`
     );
     const data = JSON.parse(await body.text());
-    // console.log("data", await data[0].pictures);
     const file = new AttachmentBuilder(data[0].pictures[0].url);
-    console.log(await file)
+    console.log(await file);
     const embed = new EmbedBuilder()
       .setColor(0xe06666)
       .setTitle(data[0].name)
       .setThumbnail(`attachment://${data[0].pictures[0].id}.jpg`)
       .addFields(
-        {name: "Age", value: data[0].age},
-        {name: "Job", value: data[0].job},
-        {name: "Description", value: data[0].description}
+        { name: "Age", value: data[0].age },
+        { name: "Job", value: data[0].job },
+        { name: "Description", value: data[0].description }
       )
-      .setFooter({text: data[0].origin})
+      .setFooter({ text: data[0].origin });
     await interaction.reply({ embeds: [embed], files: [file] });
   },
 };
