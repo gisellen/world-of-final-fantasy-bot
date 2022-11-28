f732b37e713e4e23fcea2933bda04bd783409c46
Giselle Domingo <giselle.domingo.x@gmail.com>
2022-11-28 09:54:08 -0500

    Updated descrpiption

--- commands/character.js
+++ commands/character.js
@@ -8,19 +8,20 @@ const { request } = require("undici");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("character")
-    .setDescription("Gets Lightning info.")
+    .setDescription("Gets info about a final fantasy character")
     .addStringOption(option => 
       option.setName("name").setDescription("character name").setRequired(false)
     ),
   async execute(interaction) {
     const name = interaction.options.getString("name");
     const query = new URLSearchParams({ name });
-    const { body } = await request(
+    const { statusCode, body } = await request(
       `https://www.moogleapi.com/api/v1/characters/search?${query}`
     );
     const data = JSON.parse(await body.text());
+    console.log(await data);
     const file = new AttachmentBuilder(data[0].pictures[0].url);
-    console.log(await file);
+
     const embed = new EmbedBuilder()
       .setColor(0xe06666)
       .setTitle(data[0].name)
